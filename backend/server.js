79f515600384cfbc57379e01767e0652d569e6d8
Mahmood Amara <mahmoodamara2@gmail.com>
2025-05-11 11:56:11 +0300

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+// server.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const pool = require('./run-init.js');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+
+// Root
+app.get('/', (req, res) => {
+  res.send('Portfolio API is running ✅');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
